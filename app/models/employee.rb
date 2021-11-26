6da58fe1fb80afe633ba44678575d2f014e38dbb
Francisco Paxtian <fizquierdo@quaxar.com>
2021-11-26 00:32:39 -0600

    add char js and Chartkick and we use faker for the test data

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,4 @@
 class Employee < ApplicationRecord  
   has_many :users
+  self.inheritance_column = nil
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,5 @@
 class Work < ApplicationRecord
   has_many :employees
+  self.inheritance_column = nil
+  
 end
